1edbd339c727a3965f7016b358f9230c4fa666a7
Abdul <arahman.champ@gmail.com>
2025-06-24 17:27:08 +0530

    Second Commit

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(10 >= 5);
+
+
+console.log(10 <= 5);
+console.log(10 == 5);
+console.log(10 != 5);
+console.log(10 === 5);
+console.log(10 !== 5);
+
+console.log("10" == 10);
+// console.log("10" === 10);
+console.log('01' > 1);
+console.log("2" > 2);
+
+console.log(null >= 0);
+console.log(null == 0);
+console.log(0 >= null);
+console.error("Hi ⬆  0 >= null");
+
+console.log(0 > null);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log(undefined === 0);
+
+console.table("⬆⏫undefined === 0");
+
